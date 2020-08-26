0ad11c724ace21fbc6b7f67e2c874ce6eb1e72e8
constantingoeldel <constantin_goeldel@t-online.de>
2020-08-26 22:53:20 +0200

    build projects from database

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Project from './Project'
+import projectContent from './db.json'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {projectContent.projects.map((project, index) => (
+        <Project
+          key={index}
+          title={project.title}
+          country={project.country}
+          description={project.description}
+          image={project.image}
+        />
+      ))}
+    </>
+  )
 }
 
-export default App;
+export default App

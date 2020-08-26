2108e26973d896969d57f1087bb7b7b6ac71cc16
constantingoeldel <constantin_goeldel@t-online.de>
2020-08-26 22:52:39 +0200

    Project Headline

---
+++ src/ProjectHeadline.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+
+export default function ProjectHeadline({ title, country }) {
+  return (
+    <Headline>
+      <H3>
+        {title} | {country}
+      </H3>
+    </Headline>
+  )
+}
+const Headline = styled.section`
+  display: flex;
+`
+const H3 = styled.h3`
+  font-weight: 300;
+  font-size: 170%;
+  padding: 10px 10px 5px 15px;
+  margin: 0;
+`

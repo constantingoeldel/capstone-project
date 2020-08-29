8d0197f6cec0451492f82f2c26372657d16a8fd5
constantingoeldel <constantin_goeldel@t-online.de>
2020-08-29 14:31:43 +0200

    additional cypress tests

--- cypress/integration/Project.spec.js
+++ cypress/integration/Project.spec.js
@@ -1,18 +1,20 @@
-describe('Project View', () => {
-  it('Displays the headlines correctly', () => {
+/* eslint-disable no-undef */
+
+describe('Project', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000')
+  })
+  it('Displays the headlines correctly', () => {
     cy.contains('ZeroClimate')
     cy.contains('Take a breather')
     cy.contains('Never Refuse Refuge')
   })
   it('Displays the countries correctly', () => {
-    cy.visit('http://localhost:3000')
     cy.contains('AUS')
     cy.contains('SWE')
     cy.contains('LIB')
   })
   it('Displays the description correctly', () => {
-    cy.visit('http://localhost:3000')
     cy.contains(
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempus erat at urna semper tempus. Cras euismod semper turpis, at luctus elit dignissim non. Curabitur id arcu in justo pellentesque.'
     )
@@ -20,5 +22,11 @@ describe('Project View', () => {
     cy.contains(
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempus erat at urna semper tempus. Cras turpis, at luctus elit'
     )
+  })
+  it('shows the pictures', () => {
+    cy.get('.Project__Img-nm2m85-1').should('be.visible')
+  })
+  it('renders enough projects', () => {
+    cy.get('.Project__Card-nm2m85-0').should('have.length.above', 10)
   })
 })

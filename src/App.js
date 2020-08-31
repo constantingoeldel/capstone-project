2702631968fd5af2c84946ad0e489924462420c2
constantingoeldel <constantin_goeldel@t-online.de>
2020-08-31 14:44:27 +0200

    Tags react to click

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Project from './components/Project'
 import mockData from './db.json'
 import TagCluster from './components/TagCluster'
 function App() {
+  const [selectedTags, setSelectedTags] = useState([])
+  const [projects, setProjects] = useState(mockData.projects)
+  useEffect(() => {
+    console.log('call')
+    filterProjects()
+
+    function filterProjects() {
+      console.log('called', selectedTags)
+      projects.forEach((project) => {
+        selectedTags.forEach((tag) => {
+          return project === tag && project.applyingTags ? project.applyingTags++ : 1
+        })
+      })
+      projects.sort(
+        (firstProject, secondProject) => firstProject.applyingTags - secondProject.applyingTags
+      )
+    }
+  }, [selectedTags])
   return (
     <>
-      <TagCluster tags={mockData.tags} />
-      {mockData.projects.map((project, index) => (
+      <TagCluster tags={mockData.tags} onTagClick={onTagClick} />
+      {projects.map((project, index) => (
         <Project
           key={index}
+          tags={project.tags}
           title={project.title}
           country={project.country}
           description={project.description}
@@ -17,6 +36,13 @@ function App() {
       ))}
     </>
   )
+  function onTagClick(tag) {
+    let set = new Set(selectedTags)
+    set = set.delete(tag) ? set : set.add(tag)
+    setSelectedTags([...set])
+    console.log('changed', selectedTags)
+  }
 }
 
 export default App
+//selectedTags.delete(tag) ? selectedTags : selectedTags.add(tag)

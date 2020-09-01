b843ef03dd17dcd0502be1d8fc97d8aa315875d3
constantingoeldel <constantin_goeldel@t-online.de>
2020-09-01 12:40:28 +0200

    Add TAgs to storybook

--- src/stories/Project.stories.js
+++ src/stories/Project.stories.js
@@ -8,6 +8,7 @@ export default {
 }
 export const LongDescription = () => (
   <Project
+    tags={['Social Business', 'In-Person-Support', 'Non-Profit']}
     title='Take a breather'
     country='AUS'
     description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempus erat at urna semper tempus. Cras euismod semper turpis, at luctus elit dignissim non. Curabitur id arcu in justo pellentesque.'
@@ -16,6 +17,7 @@ export const LongDescription = () => (
 )
 export const ShortDescription = () => (
   <Project
+    tags={['Legal Advice', 'Technology', 'Environment']}
     title='Take a breather'
     country='AUS'
     description='Lorem ipsum doloruelit dignissim non. Curabitur id arcu in justo pellentesque.'
@@ -24,6 +26,7 @@ export const ShortDescription = () => (
 )
 export const LongTitle = () => (
   <Project
+    tags={['Social Business', 'In-Person-Support', 'People in need']}
     title='Never Refuse Refuge'
     country='AUS'
     description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempus eelit dignissim non. Curabitur id arcu in justo pellentesque.'

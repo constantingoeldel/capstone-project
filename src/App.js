882ca165778ade704a438954b3117fb00bf4d23f
constantingoeldel <constantin_goeldel@t-online.de>
2020-09-03 12:43:51 +0200

    relocating functions into utils.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React, { useState, useEffect } from 'react'
 import Project from './components/Project'
 import mockData from './db.json'
 import TagCluster from './components/TagCluster'
-import { sortByTags } from './utils'
+import { sortByTags, filterBySearch } from './utils'
 import Search from './components/Search'
-import fuzzysearch from 'fuzzysearch'
 
 export default function App() {
   const [selectedTags, setSelectedTags] = useState([])
@@ -29,8 +28,6 @@ export default function App() {
     setSelectedTags([...set])
   }
   function onSearch(event) {
-    setSearchTerm(
-      mockData.projects.filter((project) => fuzzysearch(event.target.value, project.title) === true)
-    )
+    setSearchTerm(filterBySearch(event, mockData))
   }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import fuzzysearch from 'fuzzysearch'
+
 export function sortByTags(tags = [], source = [{}]) {
   let projects = source
     .map((project) => {
@@ -15,4 +17,12 @@ export function sortByTags(tags = [], source = [{}]) {
     )
     .filter((project) => tags.length === 0 || project.applyingTags.size > 0)
   return projects
+}
+export function filterBySearch(event, source) {
+  return source.projects.filter(
+    (project) =>
+      fuzzysearch(event.target.value, project.title) ||
+      fuzzysearch(event.target.value, project.country) ||
+      fuzzysearch(event.target.value, project.description) === true
+  )
 }

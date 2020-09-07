9c64100dfe7ec1e4c9656f29847efdfacaf0f856
constantingoeldel <constantin_goeldel@t-online.de>
2020-09-07 16:08:45 +0200

    add overlay functionality

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import TagCluster from './components/TagCluster'
 import { sortByTags, filterBySearch } from './utils'
 import Search from './components/Search'
 import Counter from './components/Counter'
+import DetailOverlay from './components/DetailOverlay'
 
 export default function App() {
   const [selectedTags, setSelectedTags] = useState([])
   const [projects, setProjects] = useState([])
   const [searchTerm, setSearchTerm] = useState(mockData.projects)
+  const [detailedProject, setDetailedProject] = useState(null)
   useEffect(() => {
     setProjects(sortByTags(selectedTags, searchTerm))
   }, [selectedTags, searchTerm])
@@ -18,11 +20,18 @@ export default function App() {
       <Search onSearch={onSearch} />
       <Counter firstInt={projects.length} secondInt={mockData.projects.length} />
       <TagCluster tags={mockData.tags} onTagClick={onTagClick} />
+      {detailedProject && <DetailOverlay project={detailedProject} onBack={hideDetails} />}
       {projects.map((project, index) => (
-        <Project key={index} data={project} />
+        <Project key={index} data={project} onClick={showDetails} />
       ))}
     </>
   )
+  function showDetails(project) {
+    setDetailedProject(project)
+  }
+  function hideDetails() {
+    setDetailedProject(null)
+  }
   function onTagClick(tag) {
     let set = new Set(selectedTags)
     set = set.delete(tag) ? set : set.add(tag)
